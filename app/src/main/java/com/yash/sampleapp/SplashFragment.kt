1c4e548fccf0942fddab92e690d195ab05f6f7d8
Yash Tandon <yashtandon007@gmail.com>
2022-05-22 23:53:28 +0530

    first commit

---
+++ app/src/main/java/com/yash/sampleapp/SplashFragment.kt
@@ -0,0 +1,34 @@
+package com.yash.sampleapp
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+
+class SplashFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+            Handler().postDelayed(Runnable {
+                findNavController().navigate(R.id.action_splashFragment_to_dashboardFrag)
+            },4000)
+
+    }
+
+
+}
